a0a5ca8c73b5f72672f00f98cef3ab4cbe628f70
Mariell Hoversholm <proximyst@proximyst.com>
2020-09-13 18:21:10 +0200

    Document new interface and methods

---
+++ src/main/java/com/proximyst/sewer/loadable/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Loadable types.
+ * <p>
+ * This defines the {@link com.proximyst.sewer.loadable.Loadable Loadable} type for dynamically loading state using an
+ * existing {@link com.proximyst.sewer.SewerSystem SewerSystem}.
+ */
+package com.proximyst.sewer.loadable;
--- src/main/java/com/proximyst/sewer/piping/ImmediatePipeHandler.java
+++ src/main/java/com/proximyst/sewer/piping/ImmediatePipeHandler.java
@@ -3,13 +3,23 @@ package com.proximyst.sewer.piping;
 import java.util.concurrent.CompletableFuture;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
+/**
+ * The handler of a pipe.
+ *
+ * @param <Input>  The input this handler may accept.
+ * @param <Output> The output this handler may accept.
+ */
 @FunctionalInterface
 public interface ImmediatePipeHandler<Input, Output> extends PipeHandler<Input, Output> {
   static <Input, Output> ImmediatePipeHandler<Input, Output> of(@NonNull ImmediatePipeHandler<Input, Output> handler) {
     return handler;
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
+  @NonNull
   default CompletableFuture<Output> flow(Input input) {
     try {
       return CompletableFuture.completedFuture(flowImmediately(input));
@@ -20,5 +30,11 @@ public interface ImmediatePipeHandler<Input, Output> extends PipeHandler<Input, 
     }
   }
 
+  /**
+   * Flow the input through this handler for an output.
+   *
+   * @param input The input expected to flow through this handler to produce an output.
+   * @return The output created by this handler.
+   */
   Output flowImmediately(Input input) throws Exception;
 }
